820d4506c87929abe1eb4a01db570060b755c0b4
FROSTYLAN <castillo089frosty@gmail.com>
2022-06-28 21:48:44 -0500

    command server and user

--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ const { token } = require("../config.json");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.once("ready", () => {
+client.on("ready", () => {
   console.log("conectado!");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "server") {
+    await interaction.reply(
+      `${interaction.guild.name}
+      Total members: ${interaction.guild.memberCount}
+      Creation date: ${interaction.guild.createdAt}
+      Verification level: ${interaction.guild.verificationLevel}`
+    );
+  } else if (commandName === "user") {
+    await interaction.reply(
+      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
+    );
+  }
 });
 
 client.login(token);
